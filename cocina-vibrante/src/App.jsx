4c270ca866b06cbbe70ebdef2e38b0ef10f29a81
kantojoey <116197161+kantojoey@users.noreply.github.com>
2026-05-19 18:11:22 -0700

    Added react router and started building header

--- cocina-vibrante/src/App.jsx
+++ cocina-vibrante/src/App.jsx
@@ -1,6 +1,34 @@
 import './App.css'
+import { Navigate, Route, Routes } from 'react-router'
+import Header from './components/common/Header'
+import HomePage from './components/pages/HomePage'
+import AboutPage from './components/pages/AboutPage'
+import MenuPage from './components/pages/MenuPage'
+import OrderPage from './components/pages/OrderPage'
+import ContactPage from './components/pages/ContactPage'
 
 function App() {
+
+
+return(
+    <div>
+
+    <Header />
+    <Routes>
+        <Route path="/" element={<HomePage/>}/>
+
+        <Route path="*" element={<Navigate to="/"/>}/>
+
+        <Route path="/about" element={<AboutPage/>}/>
+
+        <Route path="/menu" element={<MenuPage/>}/>
+
+        <Route path="/order" element={<OrderPage/>}/>
+
+        <Route path="/contact" element={<ContactPage/>}/>
+    </Routes>
+    </div>
+)
 
 }
 
--- cocina-vibrante/src/components/common/MenuBar.jsx
+++ cocina-vibrante/src/components/common/MenuBar.jsx
@@ -1,7 +1,16 @@
 const MenuBar = () => {
     return (
         <>
-            
+            <nav className="nav-bar">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About Us</a></li>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/order">Order</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
+                </ul>
+            </nav>
+
         </>
     );
 };
